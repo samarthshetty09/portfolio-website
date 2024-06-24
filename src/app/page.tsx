1c7a3e2f0a16daf2d07405c22c33b9f024341739
Samarth Shetty <samarthmshetty9@gmail.com>
2024-06-24 18:31:20 -0400

    slide up animation to next page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-import Image from "next/image";
+'use client';
+
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 
 export default function Home() {
+
+  const [animation, setAnimation] = useState('');
+  const router = useRouter();
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    setAnimation('slide-down');
+    setTimeout(() => {
+      router.push('/main');
+    }, 500);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className={`flex min-h-screen flex-col items-center justify-between ${animation}`}>
       <div className="flex min-h-screen bg-[]">
         <div className="mx-auto">
           <div className="mx-auto mb-[40px] mt-[98px] h-[213px] w-[213px] overflow-hidden rounded-full bg-gradient-to-tr from-[#FF8660] to-[#8000FF]">
@@ -13,12 +31,13 @@ export default function Home() {
             Software <span className='text-transparent bg-clip-text bg-gradient-to-tr from-[#FF8660] to-[#8000FF]"'>Engineer</span>
           </h1>
           <p className="mx-auto mt-[40px] mb-[54px] text-[18px] font-light text-[#C5C5C5] text-center max-w-lg">
-          Hi👋 Welocme to my website, I am a Computer Science Graduate Student at North Carolina State University, with over 2 years of Industry Experience as a Full Stack Software Engineer. I have expertise in developing high performance, scalable and concurrent full stack architectures.
+            Hi👋 Welocme to my website, I am a Computer Science Graduate Student at North Carolina State University, with over 2 years of Industry Experience as a Full Stack Software Engineer. I have expertise in developing high performance, scalable and concurrent full stack architectures.
           </p>
-
-          <svg className="mx-auto animate-bounce w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-          </svg>
+          <a href="/main" onClick={handleClick} className="mt-8">
+            <svg className="mx-auto w-10 h-10 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+            </svg>
+          </a>
           {/* <div className="text-center">
             <button className="text-gray-800 inline-block px-[30px] py-[16px] mr-[18px] bg-white rounded-full font-semibold text-[22px]">Learn More</button>
             <button className="text-white inline-block px-[30px] py-[16px] bg-transparent border-2 rounded-full font-semibold text-[22px]">Download CV</button>
